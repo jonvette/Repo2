0f4497d43a9fb123d5f57cf6cefabaff1ce64553
Jonathan Vette <jonvette@comcast.net>
2018-10-24 20:40:20 -0700

    Changed another line of text

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,9 @@ namespace ConsoleApp1
     static void Main(string[] args)
     {
       Console.WriteLine("Hello World!");
+      Console.WriteLine("Hello Again!");
+      Console.ReadKey();
+      Console.WriteLine("Hello one more time!");
     }
   }
 }
